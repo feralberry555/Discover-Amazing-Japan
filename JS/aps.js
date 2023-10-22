9269c67718afb6ed76fea57124053e9860f16ecc
feralberry555 <140542691+feralberry555@users.noreply.github.com>
2023-10-23 00:22:35 +0500

    Update aps.js
    Создавала функции для кнопки menuCloseItem и для burgerItem

--- JS/aps.js
+++ JS/aps.js
@@ -1,4 +1,4 @@
-(function () {
+(function headerScroll() {
 	const header = document.querySelector('.header');
 	window.onscroll = () => { /*по скролу на странице, волняется слудущее..*/
 		if (window.pageYOffset > 50) { /*при скроле вниз на 50px выполняется следущее*/
@@ -10,3 +10,16 @@
 }()); /*с помощью этой конструкции мы можем вызвать 
          данную фукции сразу, как загрузится сайт*/
 
+//Burger handler
+
+(function burgerClick() {
+	const burgerItem = document.querySelector('.burger'); //обраемся к методу document чо бы передать блок .burger в переменную
+	const menu = document.querySelector('.header_nav')
+	const menuCloseItem = document.querySelector('.header_nav-close')
+	burgerItem.addEventListener('click', () => {
+		menu.classList.add('header_nav-active');
+	});
+	menuCloseItem.addEventListener('click', () => {
+		menu.classList.remove('header_nav-active');
+	})
+}());
